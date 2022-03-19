eb4e9bd045b02ce55bf657a57c794fe7bc6d5ab7
Lu <navarromartalucia@gmail.com>
2022-03-19 13:19:47 -0300

    Video 163

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require ('sass'));
 
 function css( done){
-    src('src/scss/app.scss') //Idnetificar el archivo .SCSS a compilar
+    src('src/scss/**/*.scss') //Idnetificar el archivo .SCSS a compilar
         .pipe(sass()) //compilarlo
         .pipe(dest('build/css')) //almacenarla en el disco
     done();
@@ -11,7 +11,7 @@ function css( done){
 //un watch para que este leyendo los cambios que sucedan en este archivo y los compile automaticamente
 
 function dev (done){
-    watch('src/scss/app.scss', css);
+    watch('src/scss/**/*.scss', css);
     done ();
 }
 
