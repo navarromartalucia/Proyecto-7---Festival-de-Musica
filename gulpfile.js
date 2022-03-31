532ee4c65d12964f97dd7ee76845cfc4c536cbc2
Marta Lucia Navarro <navarromartalucia@gmail.com>
2022-03-31 12:51:21 -0300

    v.194

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const { src, dest, watch } = require('gulp');
+
+//Imagenes
+const webp = require('gulp-webp');
+
+
+//CSS
 const sass = require('gulp-sass')(require ('sass'));
 const plumber = require('gulp-plumber');
+
+
 
 function css( done){
     src('src/scss/**/*.scss') //Idnetificar el archivo .SCSS a compilar
@@ -10,12 +18,24 @@ function css( done){
     done();
 }
 
-//un watch para que este leyendo los cambios que sucedan en este archivo y los compile automaticamente
+function versionWebp (done){
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+    done();
+}
 
+
+
+//un watch para que este leyendo los cambios que sucedan en este archivo y los compile automaticamente
 function dev (done){
     watch('src/scss/**/*.scss', css);
     done ();
 }
 
+exports.versionWebp = versionWebp;
 exports.css = css;
 exports.dev = dev;
